a22016e28be5df5bdfd5ed77402952c7720aff6e
Ryan Curry <32741719+rcurry16@users.noreply.github.com>
2023-12-17 11:30:46 -0400

    Add files via upload

---
+++ api_usage.py
@@ -0,0 +1,52 @@
+# Required packages
+# - openai
+# - pathlib
+
+# import packages
+from openai import OpenAI
+from pathlib import Path
+
+# function to intialize client and set up api key - need to fix
+def init_client():
+  api_key ='enter-api-key-here'
+  client = OpenAI(api_key=api_key)
+  return client
+
+# function to generate text using gpt-3.5-turbo
+def text_gen(input):
+  client = init_client()
+  
+  completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "You are helpful assistant. Answer the given questions."},
+      {"role": "user", "content": input}
+  ])
+  
+  print(completion.choices[0].message.content)
+
+# function to generate text using dalle-3
+def image_gen(input):
+  client = init_client()
+
+  response = client.images.generate(
+  model="dall-e-3",
+  prompt=input,
+  size="1024x1024",
+  quality="standard",
+  n=1,
+  style="vivid")
+
+  image_url = response.data[0].url
+  print(image_url)  
+
+# function to generate text to speech
+def voice_gen(text, filename):
+  client = init_client()
+  speech_file_path = f"/Users/ryancurry/Desktop/{filename}.mp3"
+  response = client.audio.speech.create(
+    model="tts-1-hd",
+    voice="onyx",
+    input=text
+)
+  response.stream_to_file(speech_file_path)
